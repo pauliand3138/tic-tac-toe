d2b0244c8786566b87a980fb2dd6b5a3960d00c7
Paul <paul.iand3138@hotmail.com>
2022-10-18 19:56:13 +0800

    Add placeMark, swapTurns and setBoardHoverClass functions

---
+++ script.js
@@ -0,0 +1,45 @@
+const X_CLASS = 'x';
+const CIRCLE_CLASS = 'circle';
+const cellElements = document.querySelectorAll('[data-cell]');
+const board = document.getElementById('board');
+let circleTurn;
+
+startGame();
+
+
+function startGame() {
+    circleTurn = false;
+    cellElements.forEach(cell => {
+        cell.addEventListener('click', handleClick, { once: true } /* Fire the onclick event only once */);
+    });
+    setBoardHoverClass();
+}
+
+function handleClick(e) {
+    const cell = e.target;
+    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
+    placeMark(cell, currentClass);
+    //Check For Win
+    //Check For Draw
+    //Switch Turns
+    swapTurns();
+    setBoardHoverClass();
+}
+
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass);
+}
+
+function swapTurns() {
+    circleTurn = !circleTurn;
+}
+
+function setBoardHoverClass() {
+    board.classList.remove(X_CLASS);
+    board.classList.remove(CIRCLE_CLASS);
+    if (circleTurn) {
+        board.classList.add(CIRCLE_CLASS);
+    } else {
+        board.classList.add(X_CLASS);
+    }
+}
